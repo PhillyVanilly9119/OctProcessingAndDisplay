8b1d55c033ed560bb976e2fe04c0772fe7e95520
Philipp <philipp.matten@gmx.de>
2020-11-22 13:57:56 +0100

    Created file containing reconstruction function (bodies)

---
+++ Reconstruction.py
@@ -0,0 +1,35 @@
+"""
+
+                File containing functions for FD-domain OCT data reconstruction
+
+        Author: @Philipp Matten - philipp.matten@meduniwien.ac.at / philipp.matten@gmx.de
+                
+                        Copyright 2020 Medical University of Vienna 
+
+"""
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+################
+### DATA I/O ###
+################
+def load_data_from_bin_file(path, dtype) :
+    pass
+
+def load_image(path, dims, dtype) :
+    pass
+
+def load_data_from_images(path, dims, dtype) :
+    pass
+
+def save_data_as_images(path, data, dims, dtype) :
+    pass
+
+def save_data_as_bin_file(path, data, save_dtype=np.uint8) :
+    pass
+
+#########################
+### PRE-FFT FUNCTIONS ###
+#########################
