bae4253c74ad9cc70fbf751a189f8efd36a7b212
Philipp <philipp.matten@gmx.de>
2020-11-22 17:55:33 +0100

    Added bodies for post FFT reconstruction and FFT functions
    Only raw bodies

--- Reconstruction.py
+++ Reconstruction.py
@@ -33,3 +33,26 @@ def save_data_as_bin_file(path, data, save_dtype=np.uint8) :
 #########################
 ### PRE-FFT FUNCTIONS ###
 #########################
+def subtract_background(data) :
+    pass
+
+def apply_windowing(data, window='gauss') :
+    pass
+
+###########
+### FFT ###
+###########
+def apply_fft(data) :
+    pass
+
+def apply_cropped_fft(data, ratio=0.5) :
+    pass
+
+##########################
+### POST-FFT FUNCTIONS ###
+##########################
+def rescale_grey_values(data, black_lvl, scale_factor) :
+    shape = np.shape(data)
+    aLen = shape[2]
+    out_data = np.zeros_like(data, dtype=np.uint8)
+    out_data = np.asarray(255 * np.divide((np.subtract(data, black_lvl)), scale_factor))
