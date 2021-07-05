a4ce1653f214f90021a6e311c2e3fd79910c48a4
Philipp <philipp.matten@gmx.de>
2021-07-05 18:22:31 +0200

    Built infrastructure to create, convertz and edit the front end functions, aka GUI (so far)

---
+++ FrontEnd/uiDesingerCompileCommandsBackup.py
@@ -0,0 +1,12 @@
+"""
+                                        ******
+        Author: @Philipp Matten - philipp.matten@meduniwien.ac.at / philipp.matten@gmx.de
+                
+                        Copyright 2020 Medical University of Vienna 
+                                        ******
+                                         
+        python -m PyQt5.uic.pyuic -x "D:\PhilippDataAndFiles\Programming\Repositories\OctProcessingAndDisplay\FrontEnd\main_window.ui" 
+        -o "D:\PhilippDataAndFiles\Programming\Repositories\OctProcessingAndDisplay\FrontEnd\main_window.py"
+     
+                                
+"""
