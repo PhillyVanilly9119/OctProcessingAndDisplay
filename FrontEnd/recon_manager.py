aac6b763732d58e16ae27698ec8b254a9cc54e3d
Philipp <philipp.matten@gmx.de>
2021-07-06 14:07:48 +0200

    created file to handle intermediate communication between front and back end

---
+++ FrontEnd/recon_manager.py
@@ -0,0 +1,19 @@
+"""
+                                        ******
+        Author: @Philipp Matten - philipp.matten@meduniwien.ac.at / philipp.matten@gmx.de
+                
+                        Copyright 2020 Medical University of Vienna 
+                                        ******
+                                         
+        >>> contains handling of reconstruction functions that are called by GUI events and actions     
+                                
+"""
+
+#TODO: change to relative path import
+sys.path.append("D:\PhilippDataAndFiles\Programming\Repositories\OctProcessingAndDisplay\Backend")
+from recon_funcs import OctReconstructionManager as REC
+
+class BackendImports() :
+    def __init__(self) -> None:
+        super().__init__()
+        self.data = REC.return_oct_cube()   
