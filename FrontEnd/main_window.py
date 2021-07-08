8963dfb8047b01310023bf940369fd1b607dde9d
Philipp <philipp.matten@gmx.de>
2021-07-08 17:00:57 +0200

    Added recon and disp of BScans and started implementing enface

--- FrontEnd/main_window.py
+++ FrontEnd/main_window.py
@@ -9,7 +9,12 @@
                                 
 """
 # global imports
+import cv2
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.core.fromnumeric import cumproduct
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 # custom imports
 sys.path.append(r"D:\PhilippDataAndFiles\Programming\Repositories\OctProcessingAndDisplay\Backend")
@@ -18,16 +23,15 @@ from recon_funcs import OctReconstructionManager
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class UiWindowDialog(object):
-    
-    # Is this contructor pointless? 
-    # Can I call setupUi somehow from this constructor?   
+
+class UiWindowDialog(object) :
     def __init__(self) -> None:
         super().__init__()
         self.IO = OctDataFileManager()
         self.REC = OctReconstructionManager()
+        self.flag_calculate_enface = False
         # print(self.IO)
-        # print(self.REC)
+        # print(self.REC)   
          
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
@@ -54,18 +58,21 @@ class UiWindowDialog(object):
         self.pushButton_close.setFont(font)
         self.pushButton_close.setDefault(True)
         self.pushButton_close.setObjectName("pushButton_close")
+        # display widget for click button options
         self.DisplayOptionsWindow = QtWidgets.QLabel(Dialog)
         self.DisplayOptionsWindow.setGeometry(QtCore.QRect(30, 600, 650, 450))
         self.DisplayOptionsWindow.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.DisplayOptionsWindow.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.DisplayOptionsWindow.setText("")
         self.DisplayOptionsWindow.setObjectName("DisplayOptionsWindow")
+        # display widget for square enface image of loaded OCT volume
         self.EnfaceDisplayWindow = QtWidgets.QLabel(Dialog)
         self.EnfaceDisplayWindow.setGeometry(QtCore.QRect(700, 650, 400, 400))
         self.EnfaceDisplayWindow.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.EnfaceDisplayWindow.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.EnfaceDisplayWindow.setText("")
         self.EnfaceDisplayWindow.setObjectName("EnfaceDisplayWindow")
+        # push button for loading OCT data - with customized font
         self.pushButton_loadOctData = QtWidgets.QPushButton(Dialog)
         self.pushButton_loadOctData.setGeometry(QtCore.QRect(1789, 880, 100, 120))
         font = QtGui.QFont()
@@ -85,9 +92,9 @@ class UiWindowDialog(object):
         self.spinBox_rightBScanWindow = QtWidgets.QSpinBox(Dialog)
         self.spinBox_rightBScanWindow.setGeometry(QtCore.QRect(970, 600, 60, 30))
         self.spinBox_rightBScanWindow.setObjectName("spinBox_rightBScanWindow")
-        self.pushButton_shwEnFace = QtWidgets.QPushButton(Dialog)
-        self.pushButton_shwEnFace.setGeometry(QtCore.QRect(1120, 830, 230, 40))
-        self.pushButton_shwEnFace.setObjectName("pushButton_shwEnFace")
+        self.pushButton_showEnFace = QtWidgets.QPushButton(Dialog)
+        self.pushButton_showEnFace.setGeometry(QtCore.QRect(1120, 830, 230, 40))
+        self.pushButton_showEnFace.setObjectName("pushButton_showEnFace")
         self.pushButton_displayAScanAtIntersection = QtWidgets.QPushButton(Dialog)
         self.pushButton_displayAScanAtIntersection.setGeometry(QtCore.QRect(1120, 890, 230, 40))
         self.pushButton_displayAScanAtIntersection.setObjectName("pushButton_displayAScanAtIntersection")
@@ -211,18 +218,26 @@ class UiWindowDialog(object):
         self.label_WindowingFunction_.setFont(font)
         self.label_WindowingFunction_.setObjectName("label_WindowingFunction_")
 
-        # set Labels and
+        # set all style elements in UI
         self.retranslateUi(Dialog)
         
         # load OCT data
         self.pushButton_loadOctData.clicked.connect(self._load_oct_data)
         # exit if close button is pressed
-        self.close_application_via_button()            
+        self.close_application_via_button()      
         """
         1. if and only if OCT data cube has been loaded 
             Options:
                 i) display reconstructed selected A-scans
         """
+        # run reconstruction for display of current pair of cross-sectional A-scans
+        self.pushButton_runReconstruction.clicked.connect(self.run_recon_for_current_settings)
+        
+        # run calculation of enface image and display
+        # self.pushButton_showEnFace.clicked.connect(self.display_enface_image) # implement efficient enface-calculation algo
+        self.pushButton_showEnFace.clicked.connect(self.overlay_BScans_with_lines) 
+        
+        self.pushButton_displayWindowingFunctions.clicked.connect(self.display_current_windowing_function)
         
         self.horizontalSlider_leftBScanWindow.valueChanged['int'].connect(self.spinBox_leftBScanWindow.setValue)
         self.spinBox_leftBScanWindow.valueChanged['int'].connect(self.horizontalSlider_leftBScanWindow.setValue)
@@ -235,7 +250,7 @@ class UiWindowDialog(object):
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
         self.pushButton_close.setText(_translate("Dialog", "Close"))
         self.pushButton_loadOctData.setText(_translate("Dialog", "Load\nOCT\nData"))
-        self.pushButton_shwEnFace.setText(_translate("Dialog", "Show en face OCT"))
+        self.pushButton_showEnFace.setText(_translate("Dialog", "Show en face OCT"))
         self.pushButton_displayAScanAtIntersection.setText(_translate("Dialog", "Display A-scan at Intersection"))
         self.pushButton_displayDispersionCurves.setText(_translate("Dialog", "Plot Dispersion Curves"))
         self.pushButton_displayWindowingFunctions.setText(_translate("Dialog", "Plot Windowing Function"))
@@ -250,10 +265,6 @@ class UiWindowDialog(object):
         self.label_WindowingFunction_.setText(_translate("Dialog", "Windowing Function"))
   
     #### Backend-connected functions ####
-
-    def run_recon_for_current_settings(self) :
-        pass
-        
     def _load_oct_data(self) -> None :
         """ loads user-selected file containing OCT data and created class-vars raw data buffer and dimensions """
         print("Loading OCT data... ")
@@ -263,6 +274,76 @@ class UiWindowDialog(object):
         self.dims_buffer_oct_raw_data = buffer_oct_raw_data.shape
         self.set_spinbox_max_values(self.dims_buffer_oct_raw_data[1], self.dims_buffer_oct_raw_data[2]) 
         self.set_bScan_slider_max_values(self.dims_buffer_oct_raw_data[1], self.dims_buffer_oct_raw_data[2]) 
+
+    def overlay_BScans_with_lines(self) :
+        # TODO: works initially but cant update position... Figure it out
+        # Enface image is a numpy array with ones for now
+        dummy_enface = np.full((400,400), 128)
+        # dummy_enface = cv2.cvtColor(dummy_enface, cv2.COLOR_GRAY2BGR)
+        q_curr_hori_scan = QtGui.QImage(dummy_enface.data.tobytes(), 
+                                        dummy_enface.shape[1], dummy_enface.shape[0], 
+                                        QtGui.QImage.Format_Indexed8)
+        # convert image file into pixmap
+        self.pixmap_image = QtGui.QPixmap(q_curr_hori_scan)
+        # create painter instance with pixmap
+        self.painterInstance = QtGui.QPainter(self.pixmap_image)
+        # set rectangle color and thickness
+        self.v_bScan_line = QtGui.QPen(QtCore.Qt.red)
+        self.v_bScan_line.setWidth(3)
+        # draw rectangle on painter
+        self.painterInstance.setPen(self.v_bScan_line)
+        self.painterInstance.drawLine(self.spinBox_leftBScanWindow.value(), 1, 
+                                      self.spinBox_leftBScanWindow.value(), 400) # get vals from from spinbox/slider values
+        # set pixmap onto the label widget
+        self.EnfaceDisplayWindow.setPixmap(self.pixmap_image)
+        self.EnfaceDisplayWindow.setScaledContents(True)
+        print("Done")        
+        
+    def display_current_windowing_function(self) :
+        pass # TODO: implement
+        # key = 'hann' # get key from drop down 
+        # curve = self.REC.create_windowing_function(self.dims_buffer_oct_raw_data[0], key=key)
+        # self.canvas_DisplayOptionsWindow = MplCanvas(self.DisplayOptionsWindow, curve)
+
+    def display_enface_image(self) :
+        # TODO: implement computationally inexpensive version of enface
+        if not self.flag_calculate_enface:
+            print("Calculating Enface")
+            
+            # self.overlay_BScans_with_lines()
+            
+            # enface = self.REC.reconstruct_buffer(self.buffer_oct_raw_data) # add disp coeffs and windowing key 
+            # q_enface = cv2.cvtColor(enface , cv2.COLOR_GRAY2BGR)
+            # q_enface = QtGui.QImage(q_enface.data.tobytes(), q_enface.shape[1], q_enface.shape[0], QtGui.QImage.Format_Indexed8)
+            # self.EnfaceDisplayWindow.setPixmap( QtGui.QPixmap(q_enface) )
+            # self.EnfaceDisplayWindow.setScaledContents(True) 
+            # self.flag_calculate_enface = True
+        else :
+            print("Enface has already been calculated")
+
+    def run_recon_for_current_settings(self) :
+        print("Recon running...")
+        # 1 reconstruct B-scans of current pair
+        # horizontal/left scan
+        curr_h_scan_idx = self.spinBox_leftBScanWindow.value()
+        curr_hori_scan = self.REC.reconstruct_buffer(self.buffer_oct_raw_data[:,curr_h_scan_idx,:]) # add disp coeffs and windowing key 
+        curr_hori_scan = cv2.cvtColor(curr_hori_scan , cv2.COLOR_GRAY2BGR)
+        q_curr_hori_scan = QtGui.QImage(curr_hori_scan.data.tobytes(),
+                                              curr_hori_scan.shape[1], curr_hori_scan.shape[0],
+                                              QtGui.QImage.Format_Indexed8)
+        self.LeftBScanWindow.setPixmap( QtGui.QPixmap(q_curr_hori_scan) )
+        self.LeftBScanWindow.setScaledContents(True) 
+        # vertical/right scan
+        curr_v_scan_idx = self.spinBox_rightBScanWindow.value()
+        curr_vert_scan = self.REC.reconstruct_buffer(self.buffer_oct_raw_data[:,:,curr_v_scan_idx]) # add disp coeffs and windowing key 
+        curr_vert_scan = cv2.cvtColor(curr_vert_scan , cv2.COLOR_GRAY2BGR)
+        q_curr_vert_scan = QtGui.QImage(curr_vert_scan.data.tobytes(),
+                                              curr_vert_scan.shape[1], curr_vert_scan.shape[0],
+                                              QtGui.QImage.Format_Indexed8)
+        self.Right_BScanWindow.setPixmap( QtGui.QPixmap(q_curr_vert_scan) )
+        self.Right_BScanWindow.setScaledContents(True) # 2 display reconstructed B-scan pain
+        
+        
         
     def set_bScan_slider_max_values(self, x_max, y_max) :
         """ Sets the values of the horizontal sliders to OCT (x,y) volume dims
