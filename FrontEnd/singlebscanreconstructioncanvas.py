c342d90bee7d2d4a583d3a4fbd110b60b383f8e5
Philipp <philipp.matten@gmx.de>
2022-01-17 07:13:31 +0100

    Added messagebox for user input after example b-scan processing

--- FrontEnd/singlebscanreconstructioncanvas.py
+++ FrontEnd/singlebscanreconstructioncanvas.py
@@ -13,11 +13,9 @@
 import os
 import cv2
 import sys
-import PIL
-# import numpy as np
 import json
 import matplotlib.pyplot as plt # debug
-from tkinter.filedialog import Tk, askopenfilename
+from tkinter import messagebox
 
 # custom imports
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'Backend')))
@@ -26,7 +24,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 
 # import backend module(s)
 from octreconstructionmanager import OctReconstructionManager
     
-def process_test_buffer(manual_json_selection: bool=False) :
+
+def process_test_buffer(manual_json_selection: bool) -> bool:
     Rec = OctReconstructionManager()
     path = Rec._tk_file_selection()#"Please select a file for testing the reconstruction parameters:")
     raw_data = Rec.reshape_oct_volume(Rec.load_bin_file(path), Rec.get_oct_volume_dims(path)[0])
@@ -48,25 +47,32 @@ def process_test_buffer(manual_json_selection: bool=False) :
     recon_data = cv2.resize(recon_data, (900, 1600))
     # show recon data to user
     fig = plt.figure(figsize=(5, 10))
-    # recon_data = recon_data.resize(recon_data, (1600, 900))
     ax = fig.add_axes([0, 0, 1, 1])
     ax.axis('off')
+    plt.title("Reconstructed and reshaped b-Scan", fontsize=15)
     ax.imshow(recon_data, cmap='gray')
-    plt.show()
+    plt.draw()
+    plt.pause(0.001)
     # display information and ask user if they are satisfied with the reconstructed data
-    
-    ## TODO: think how the functions can be used ideally to reach goal of
-    ## reconstructing entire folder with raw b-Scans
-    # ask user to open folder with all raw b-Scans 
-    
-    # Loop through and process all b-scans
+    response = messagebox.askyesno("Ask Question", "Reconstruction with current parameters has finished\nPlease select \'Yes\' if you want to continue the reconstruction for all files or \'No\' if you want to adjust the reconstruction parameters")
+    if response == True:
+        print("[INFO:] PROCESSING - Continuing with reconstruction of all buffers in folder...")
+        return True, path
+    elif response == False:
+        print("\'No\' was selected - please adjust parameters in *.JSON-file")
+        return False, path
 
-def run():
-    process_test_buffer()
+def run(manual_json_selection: bool):
+    # process and display 
+    res, path = process_test_buffer(manual_json_selection)
+    if res == False:
+        return
+    # call processing function to recon entre folder 
     
 
 # for testing and debugging purposes
 if __name__ == '__main__' :
-    print("[INFO:] Running from < singlebscanreconstructioncanvas.py > ...")
-    run()
+    print("[INFO:] Running from <singlebscanreconstructioncanvas.py > ...")
+    manual_json_selection = False
+    run(manual_json_selection)
 
